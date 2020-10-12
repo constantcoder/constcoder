cae32a32514683769e2105131e26d9e1495bb27e
Constant Coder <garrett@constantcoder.io>
2020-10-12 11:13:31 -0500

    📦 NEW: First version

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log(`
+Constant Coder
+
+Constant Coder is Garrett Lovell. A software engineer for Quad/Tractor Supply Co. Based in Nashville, TN I have a badass wife and dog and trying to help other people get better by giving tip and tricks for coding and the industry. Follow me on my social media stuff.
+
+Twitter:  https://www.twitter.com/CoderConstant
+GitHub:   https://github.com/constantcoder
+Blog:     https://www.constantcoder.io
+
+`)
