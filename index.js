8243953f367266d78dbe2fbcda79094903b45754
Constant Coder <garrett@constantcoder.io>
2020-10-13 11:35:55 -0500

    📦 NEW: Added colors and styling

--- index.js
+++ index.js
@@ -2,28 +2,28 @@
 const pkgJSON = require('./package.json')
 const welcome = require('cli-welcome')
 const chalk = require('chalk')
+const twitterClr = chalk.hex(`#1da1f2`).bold.inverse
+const githubClr = chalk.hex(`#6cc644`).bold.inverse
+const blogClr = chalk.hex(`#6e5494`).bold.inverse
+const greyText = chalk.hex('#7F7F7F').bold
+const italic = chalk.italic
 
 welcome({
-  title: pkgJSON.name,
+  title: `Constant Coder`,
   version: pkgJSON.version,
   description: pkgJSON.description,
-  tagLine: `by Constant Coder`,
   bgColor: `#8AFF80`,
   color: `#0B0D0F`,
   bold: true,
   clear: true,
 })
 
-// Clears the console.
-console.log(`
-${chalk.blue(`Constant Coder`)}
-
-${chalk.dim(
-  `Constant Coder is Garrett Lovell. A software engineer for Quad/Tractor Supply Co. Based in Nashville, TN I have a badass wife and dog and trying to help other people get better by giving tip and tricks for coding and the industry. Follow me on my social media stuff.`,
+console.log(`${italic(
+  `Constant Coder is Garrett Lovell. A software engineer for Quad/Tractor Supply Co. Based in Nashville, TN I have a badass wife and dog and trying to help other people get better by giving tips and tricks for coding and getting in the industry. Follow me on my social media stuff.`,
 )}
 
-Twitter:  https://www.twitter.com/CoderConstant
-GitHub:   https://github.com/constantcoder
-Blog:     https://www.constantcoder.io
+${twitterClr(` Twitter `)}  ${greyText(`https://www.twitter.com/CoderConstant`)}
+${githubClr(` GitHub `)}   ${greyText(`https://github.com/constantcoder`)}
+${blogClr(` Blog `)}     ${greyText(`https://www.constantcoder.io`)}
 
 `)
