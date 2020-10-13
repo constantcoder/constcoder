085e331ab808ceecab3f1d70ae0b7628532e89c6
Constant Coder <garrett@constantcoder.io>
2020-10-13 14:00:20 -0500

    👌 IMPROVE: Added alert icons

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const chalk = require('chalk')
 const twitterClr = chalk.hex(`#1da1f2`).bold.inverse
 const githubClr = chalk.hex(`#6cc644`).bold.inverse
 const blogClr = chalk.hex(`#6e5494`).bold.inverse
-const greyText = chalk.hex('#7F7F7F').bold
+const lightGreyText = chalk.hex(`#C6C6C6`)
+const greyText = chalk.hex(`#7F7F7F`)
 const italic = chalk.italic
+
+// Alerts
+const sym = require('log-symbols')
+const success = chalk.green.bold
+const info = chalk.blue.bold
+const warning = chalk.keyword(`yellow`).bold
+const error = chalk.red.bold
 
 welcome({
   title: `Constant Coder`,
@@ -22,8 +30,24 @@ console.log(`${italic(
   `Constant Coder is Garrett Lovell. A software engineer for Quad/Tractor Supply Co. Based in Nashville, TN I have a badass wife and dog and trying to help other people get better by giving tips and tricks for coding and getting in the industry. Follow me on my social media stuff.`,
 )}
 
-${twitterClr(` Twitter `)}  ${greyText(`https://www.twitter.com/CoderConstant`)}
-${githubClr(` GitHub `)}   ${greyText(`https://github.com/constantcoder`)}
-${blogClr(` Blog `)}     ${greyText(`https://www.constantcoder.io`)}
+${twitterClr(` Twitter `)}  ${lightGreyText(
+  `https://www.twitter.com/CoderConstant`,
+)}
+${githubClr(` GitHub `)}   ${lightGreyText(`https://github.com/constantcoder`)}
+${blogClr(` Blog `)}     ${lightGreyText(`https://www.constantcoder.io`)}
+`)
 
+console.log(`
+  ${sym.success} ${success(`SUCCESS`)} - ${greyText(
+  `Thank you for checking out my CLI.`,
+)}
+  ${sym.info} ${info(`INFO`)} - ${greyText(
+  `I really enjoy creating CLI tools with NoseJS.`,
+)}
+  ${sym.warning} ${warning(`WARNING`)} - ${greyText(
+  `Look...I'm new to this so cut me some slack.`,
+)}
+  ${sym.error} ${error(`ERROR`)} - ${greyText(
+  `I just wanted to add one more alert here.`,
+)}
 `)
